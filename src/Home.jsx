97d47d91f882c377a8dc32e9bf67cc34cfc2302a
Erwin Ramadhan <erwinramadhan35@gmail.com>
2021-03-26 17:39:48 +0700

    calculator input init

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,17 @@
 import { Component } from 'react'
 import React from 'react'
-import { Button } from 'reactstrap'
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
 import './home.css';
 
 class Home extends Component {
     constructor() {
         super()
         this.state = {
-            score: 0
+            score: 0,
+            x: null,
+            y: null,
+            operator: null,
+            result: null
         }
     }
 
@@ -29,6 +33,40 @@ class Home extends Component {
         })
     }
 
+    handleInput = (event) => {
+        console.log(event.target.name)
+        console.log(event.target.value)
+        const { name, value } = event.target
+        this.setState({
+            [name]: value
+        })
+        
+        const { x, y, operator } = this.state
+        
+        switch (operator) {
+            case '+':
+                return this.setState(prevState => {return{
+                    result: prevState.result = +x + +y
+                }})
+            case '-':
+                this.setState({
+                    result: x - y
+                })
+                break;
+            case '*':
+                this.setState({
+                    result: x * y
+                })
+                break;
+            case '/':
+                this.setState({
+                    result: x / y
+                })
+                break;
+                default:return this.state
+        }
+    }
+
     render() {
         return (
             <>
@@ -36,6 +74,36 @@ class Home extends Component {
                 <Button color="primary" onClick={this.handleScoreTambah}>Tambah</Button>
                 <Button color="danger" onClick={this.handleScoreReset}>Reset</Button>
                 <Button color="warning" onClick={this.handleScoreKurang}>Kurang</Button>
+                <hr/>
+                <Form style={{ width: "50%", margin: "0 auto" }}>
+                    <FormGroup>
+                        <Label>X</Label>
+                        <Input 
+                            type="number"
+                            name="x"
+                            placeholder="Masukan nilai X"
+                            onChange={(event) => this.handleInput(event)}
+                        />
+                    </FormGroup>
+
+                    <Input type="select" name="operator" onChange={(event) => this.handleInput(event)}>
+                        <option value="+">+</option>
+                        <option value="-">-</option>
+                        <option value="*">*</option>
+                        <option value="/">/</option>
+                    </Input>
+
+                    <FormGroup>
+                        <Label>Y</Label>
+                        <Input
+                            type="number"
+                            name="y"
+                            placeholder="Masukan nilai Y"
+                            onChange={(event) => this.handleInput(event)}
+                        />
+                    </FormGroup>
+                </Form>
+                <h2>{this.state.result}</h2>
             </>
         )
     }
